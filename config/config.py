e2c2c982035e5b6018c5a42819253730bac010bc
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-17 16:06:39 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -96,7 +96,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheRishuCoder/VIP-1MUSIC",
+    "https://github.com/TheRishuCoder/VIP-MUUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
